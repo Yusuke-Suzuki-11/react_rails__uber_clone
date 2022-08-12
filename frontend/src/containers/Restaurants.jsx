7a65a17729c863cc54a389d9c6dd49a1b7dfeaf5
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-12 18:21:15 +0900

    マテリアルUI導入

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,29 @@
 import { Fragment, useEffect } from 'react';
 import { fetchRestaurants } from '../apis/restaurants';
+import styled from 'styled-components';
+
+
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+    height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+    text-align: center;
+`;
+
+const MainCover = styled.img`
+    height: 600px;
+`;
+
 
 
 export const Restaurants = () => {
